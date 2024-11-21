31698a01faf070f8088d8cc224291dbe853c48ba
Mukhammadsidik Ibrokhimov <mukibrok@c4b9c1.42berlin.de>
2024-11-21 17:05:43 +0100

    ft_printf

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mukibrok <mukibrok@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 16:00:42 by mukibrok          #+#    #+#             */
+/*   Updated: 2024/11/21 16:03:36 by mukibrok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+
+# define FT_PRINTF_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+#endif
