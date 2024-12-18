7b57c14ee1b0236597cde167e478ec4b690e01cc
Mukhammad Ibrokhimov <121773164+mukhammadsiddiq@users.noreply.github.com>
2024-12-18 21:14:56 +0100

    Add files via upload

---
+++ Puhs Swap/Push Swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Push Swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mukibrok <mukibrok@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 16:58:36 by mukibrok          #+#    #+#             */
+/*   Updated: 2024/12/17 17:05:21 by mukibrok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
+
+char	**ft_split(char const *s, char c);
+
+#endif
